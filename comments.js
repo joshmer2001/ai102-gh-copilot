0fde0479d8491a4d6d8384fa52659df00c2d5a39
freesmoke24 <101024683+freesmoke24@users.noreply.github.com>
2023-09-17 17:14:21 +0000

    thrid commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+//create web server
+//run this file using node comments.js
+//open browser and type http://localhost:8080
+//to stop the server press ctrl+c
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+
+var comments = [];
